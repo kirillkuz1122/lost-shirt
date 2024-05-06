148c44f0c526649fd09cae38c7da43913a4adc82
Кирилл Кузмичев <kirill2011kuz@gmail.com>
2024-05-06 15:41:59 +0500

    update

---
+++ scripts/about-product__menu.js
@@ -0,0 +1,23 @@
+(function() {
+    const elem = document.querySelector('.about-product__list');
+    const iso = new Isotope( elem, {
+        temSelector: '.about-product__item', 
+        filter: '.desc',
+    });
+
+    const menubtn = document.querySelectorAll('.menu__link');
+
+    menubtn.forEach(function(btn) {
+        btn.addEventListener('click', function(e) {
+            e.preventDefault();
+            const filtername = btn.getAttribute('data-filter');
+            menubtn.forEach(function(link) {
+                link.classList.remove('menu__link--active');
+            })
+            btn.classList.add('menu__link--active');
+            iso.arrange({
+                filter: `.${filtername}`,
+            })
+        })
+    })
+}())
